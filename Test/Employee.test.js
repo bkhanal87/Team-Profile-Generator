b1b72a4da4ae3aa7cdc4b68ade1d8978eaccf05f
Basu Khanal <87610840+bkhanal87@users.noreply.github.com>
2021-09-27 14:38:58 -0400

    added test parameters to Employee.tests.js

---
+++ Test/Employee.test.js
@@ -0,0 +1,45 @@
+const { TestWatcher } = require("@jest/core");
+const Employee = require("../lib/Employee");
+
+const Employee1 = new Employee('John', 3, 'john793a@yahoo.com');
+
+test("can produce instance of Employee", () => {
+    expect(Employee1 instanceof Employee).toEqual(true);
+
+});
+
+test("can set name from the constructor argument", () => {
+    expect(Employee1.name).toEqual('John');
+
+});
+
+test("can set id from the constructor argument", () => {
+    expect(Employee1.id).toEqual(3);
+
+});
+
+test("can set email from the constructor argument", () => {
+    expect(Employee1.email).toEqual('john793a@yahoo.com');
+
+});
+
+test("can get name from getName()", () => {
+    expect(Employee1.getName()).toEqual('john');
+
+});
+
+test("can get id from getID()", () => {
+    expect(Employee1.getID).toEqual(3);
+
+});
+
+test("getRole() need to return \"Employee\"", () => {
+    expect(Employee1.getRole()).toEqual("Employee");
+
+});
+
+test("can produce instance of Employee", () => {
+    expect(Employee1 instanceof Employee).toEqual(true);
+
+});
+
